911a878afa081ff1e6b5371b7efeaeee4e158649
Shixiong Wang <62227601+Spratm-Asleaf@users.noreply.github.com>
2021-11-01 16:20:02 +0800

    Delete [3] t-distribution directory

--- [3] t-distribution/trandn.m
+++
@@ -1,8 +0,0 @@
-
-% student-t distributed noise
-function samples = trandn(m,n,dof)
-    variance = dof/(dof-2);
-    coef = 1/sqrt(variance);
-    
-    samples = coef * trnd(dof,m,n);     % "samples" has zero mean and unit variance
-end
